ab48270c0e218f2d5e787571d17a652c4ca88134
mamlaki <melekredwan@icloud.com>
2023-09-24 04:13:34 -0600

    Initial Commit

---
+++ DailyDrift/DailyDriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDriftApp.swift
+//  DailyDrift
+//
+//  Created by Melek Redwan on 2023-09-24.
+//
+
+import SwiftUI
+
+@main
+struct DailyDriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
