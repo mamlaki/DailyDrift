e88468084c55596ba4a4d3730396a10d1299661c
mamlaki <melekredwan@icloud.com>
2023-09-24 19:56:03 -0600

    Added placeholder text to new entry description.

--- DailyDrift/NewEntryView.swift
+++ DailyDrift/NewEntryView.swift
@@ -15,7 +15,16 @@ struct NewEntryView: View {
     var body: some View {
         Form {
             TextField("Title", text: $title)
-            TextEditor(text: $content)
+           
+            ZStack(alignment: .topLeading) {
+                if content.isEmpty {
+                    Text("Entry")
+                        .foregroundStyle(Color(UIColor.placeholderText))
+                        .padding(.top, 8)
+                }
+                TextEditor(text: $content)
+            }
+            
             Button("Save") {
                 let newEntry = Entry(date: Date(), title: title, content: content)
                 entryStore.add(newEntry)
